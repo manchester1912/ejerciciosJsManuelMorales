7fb6fd7d1c9fb002e963792a220eba4cac891f9e
manchester1912 <manuelbastidasba@gmail.com>
2024-05-07 21:54:33 -0300

    se crea repositorio separado para eventos, ya que el general da error con esta carpeta

---
+++ eventos.js
@@ -0,0 +1,14 @@
+// let estatura = parseInt(prompt("Ingrese su Estatura"))
+
+// let peso = parseInt(prompt("Ingrese su Peso"))
+
+// let estaturaMetros = estatura / 100
+
+// console.log(estaturaMetros)
+
+// let indiceMasaCorporal = (peso/((estaturaMetros)*(estaturaMetros))) 
+
+
+// console.log(indiceMasaCorporal)
+
+
